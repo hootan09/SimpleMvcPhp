555c24f0f39f36634c9f64c6118aecde78194a88
hootan09 <hootan09@yahoo.com>
2017-02-19 09:42:56 -0800

    new sync
    add new stuff

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -10,4 +10,9 @@ class Controller
     require_once '../app/models/' . $model . '.php';
     return new $model();
   }
+
+  public function view($view , $data = [])
+  {
+    require_once '../app/views/' . $view .'.php';
+  }
 }
